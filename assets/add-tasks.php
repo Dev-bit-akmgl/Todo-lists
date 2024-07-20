8eb6f0c3765229ef9846babe6f21031724607bac
Dev-bit-akmgl <ordinaryanfal.mughal@gmail.com>
2024-07-20 18:13:01 +0500

    insert form

--- assets/add-tasks.php
+++ assets/add-tasks.php
@@ -17,15 +17,25 @@
                 ?>
                 <!-- Page content-->
                 <div class="container-fluid">
-                    <div class="col-12 my-4 shadow rounded">
+                    <div class="col-11 my-4 shadow rounded">
                         <div class="col-12 text-center display-4 fw-bold pb-4 rounded"><?php echo $pageName; ?></div>
                         <form action="">
-                        <input type="text" name="title" class="col-3 px-3 py-2 mx-5 rounded" placeholder="Home Work">
-                        <input type="file" name="image" class="col-3 px-3 py-2 mx-5 rounded" placeholder="">
-                        <input type="date" name="expire-date" class="col-3 px-3 py-2 mx-5 rounded ">
-                        <textarea name="describe" id="" class="col-11 rounded px-2 mx-5 mt-4" placeholder="Type Description..." ></textarea>
-                         <button class="btn btn-primary mt-3 mx-5 mb-4">Submit</button>   
-                    
+                            <div class="col-12 d-flex">
+                                <div class="col-3">
+                                    <input type="text" class="col-12 rounded px-3 py-2 border border-secondary mx-5" placeholder="Home work" name="title">
+                                </div>
+                                <div class="col-3 mx-5">
+                                    <input type="file" class="form-control mx-5" name="image">
+                                </div>
+                                <div class="col-3">
+                                    <input type="date" class="col-12 rounded px-3 py-2 border border-secondary mx-5" name="expire-date">
+                                </div>
+                            </div> 
+                            <div class="col-12">
+                                <textarea name="description"  class="col-10 rounded mt-2 mx-5 px-3" placeholder="Type description..."></textarea>
+                            </div>
+                            
+                            <button class="btn btn-primary mt-2 mx-5 mb-4">Submit</button>
                         </form>
                     </div>
                     
