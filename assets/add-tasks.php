94cbf7cb3ed830ab7f38d77978222ecbc152970b
Dev-bit-akmgl <ordinaryanfal.mughal@gmail.com>
2024-07-20 17:49:43 +0500

    bug fixed

--- assets/add-tasks.php
+++ assets/add-tasks.php
@@ -17,7 +17,17 @@
                 ?>
                 <!-- Page content-->
                 <div class="container-fluid">
-                    <h1 class="mt-4"><?php echo $pageName; ?></h1>
+                    <div class="col-12 my-4 shadow rounded">
+                        <div class="col-12 text-center display-4 fw-bold pb-4 rounded"><?php echo $pageName; ?></div>
+                        <form action="">
+                        <input type="text" name="title" class="col-3 px-3 py-2 mx-5 rounded" placeholder="Home Work">
+                        <input type="file" name="image" class="col-3 px-3 py-2 mx-5 rounded" placeholder="">
+                        <input type="date" name="expire-date" class="col-3 px-3 py-2 mx-5 rounded ">
+                        <textarea name="describe" id="" class="col-11 rounded px-2 mx-5 mt-4" placeholder="Type Description..." ></textarea>
+                         <button class="btn btn-primary mt-3 mx-5 mb-4">Submit</button>   
+                    
+                        </form>
+                    </div>
                     
                 </div>
             </div>
--- assets/inc/sidebar.php
+++ assets/inc/sidebar.php
@@ -3,7 +3,8 @@
                 <div class="list-group list-group-flush">
                     <a class="list-group-item list-group-item-action list-group-item-light p-3 <?php echo($pageName == "Dashboard") ?"active-page" : "" ?>" href="../assets/dashboard.php">Dashboard</a>
                     <a class="list-group-item list-group-item-action list-group-item-light p-3 <?php echo($pageName == "Tasks") ?"active-page" : "" ?>" href="../assets/tasks.php">Tasks</a>
-                    <a class="list-group-item list-group-item-action list-group-item-light p-3 <?php echo($pageName == "Add Tasks") ?"active-page" : "" ?>" href="../asstes/add-tasks.php">Add tasks</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3 <?php echo($pageName == "Add Tasks") ?"active-page" : "" ?>" href="../assets/add-tasks.php">Add Tasks</a>
+                    
                     
                 </div>
  </div>
