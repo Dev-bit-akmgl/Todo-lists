6a273c8b3f741bf7e2cf0f58446fd3c569a5b22c
Dev-bit-akmgl <ordinaryanfal.mughal@gmail.com>
2024-07-22 17:07:28 +0500

    bug fixed

--- assets/add-tasks.php
+++ assets/add-tasks.php
@@ -42,7 +42,7 @@
                 <div class="container-fluid">
                     <div class="col-11 my-4 shadow rounded">
                         <div class="col-12 text-center display-4 fw-bold pb-4 rounded"><?php echo $pageName; ?></div>
-                        <form action="push">
+                        <form action="" method="post" >
                             <div class="col-12 d-flex">
                                 <div class="col-3">
                                     <input type="text" class="col-12 rounded px-3 py-2 border border-secondary mx-5" placeholder="Home work" name="title">
@@ -58,7 +58,7 @@
                                 <textarea name="description"  class="col-10 rounded mt-2 mx-5 px-3" placeholder="Type description..."></textarea>
                             </div>
                             
-                            <button class="btn btn-primary mt-2 mx-5 mb-4" name="btn-submit">Submit</button>
+                            <button class="btn btn-primary mt-2 mx-5 mb-4" name="btn-submit" value="submit">Submit</button>
                         </form>
                     </div>
                     
