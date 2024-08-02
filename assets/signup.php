8d8050048871f8d26c55a3db64eb4f2975d2e651
Dev-bit-akmgl <ordinaryanfal.mughal@gmail.com>
2024-08-03 02:16:05 +0500

    did notning

--- assets/signup.php
+++ assets/signup.php
@@ -8,12 +8,12 @@ require_once ("./inc/top.php");
         <div class="col-10 mx-auto">
             <form action="" method="post">
                 <input type="text" class="form-control my-3" name="username" placeholder="Username">
-                <input type="email" class="form-control my-3" name="email" placeholder="Email">
-                <input type="email" class="form-control my-3" name="re-email" placeholder="Retype Email">
+                <input type="email" class="form-control my-3" name="email" placeholder="Email" id="email">
+                <input type="email" class="form-control my-3" name="re-email" placeholder="Retype Email" id="re-email">
                 <input type="password" class="form-control my-3" name="password" placeholder="Password">
                 <input type="password" class="form-control my-3" name="re-password" placeholder="Retype Password">
 
-                <button class="btn btn-primary mb-3" name="btn-register" value="submit">Register</button>
+                <button class="btn btn-primary mb-3" name="btn-register" value="submit" id="btn">Register</button>
             </form>
 
             <p class="text-center">Already have an account? <a href="index.php">Login</a></p>
