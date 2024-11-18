8790ac5021e3511892144e77eef9788352313cf1
Dev-bit-akmgl <ordinaryanfal.mughal@gmail.com>
2024-11-18 08:50:16 +0500

    Did some minor changes in navbar

--- assets/inc/navbar.php
+++ assets/inc/navbar.php
@@ -7,13 +7,8 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
 
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
-                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Settings</a>
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="?action=logout">Logout</a>
-
-                    </div>
+                <li class="nav-item ">
+                    <a class="dropdown-item" href="?action=logout">Logout</a>
                 </li>
             </ul>
         </div>
